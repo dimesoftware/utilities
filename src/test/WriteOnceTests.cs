2b1abef8ec5cf98a6a1c8a3f49a46b2d0f0906ae
Hendrik Bulens <hendrik.bulens@gmail.com>
2018-12-14 17:00:59 +0100

    Refactoring from Dime.Scheduler

---
+++ src/test/WriteOnceTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using Xunit;
+
+namespace Dime.Utilities.Core.Tests
+{
+    public class WriteOnceTests
+    {
+        [Fact]
+        [Trait("Category", "WriteOnce")]
+        public void WriteOnce_SetsAndGets_ReturnsInstance()
+        {
+            WriteOnce<string> txt = new WriteOnce<string> { Value = "Hello world" };
+            Assert.True(txt.Value == "Hello world");
+            Assert.True(txt.ToString() == "Hello world");
+            Assert.True(txt.ValueOrDefault == "Hello world");
+            Assert.True((string)txt == "Hello world");
+        }
+
+        [Fact]
+        [Trait("Category", "WriteOnce")]
+        public void WriteOnce_GetsBeforeSets_ThrowsError()
+        {
+            WriteOnce<string> txt = new WriteOnce<string>();
+            Assert.Throws<InvalidOperationException>(() => txt.Value == "Hello world");
+        }
+
+        [Fact]
+        [Trait("Category", "WriteOnce")]
+        public void WriteOnce_SetsTwice_ThrowsError()
+        {
+            WriteOnce<string> txt = new WriteOnce<string> { Value = "Hello world" };
+            Assert.True(txt.Value == "Hello world");
+            Assert.Throws<InvalidOperationException>(() => txt.Value = "Hello again");
+        }
+    }
+}
