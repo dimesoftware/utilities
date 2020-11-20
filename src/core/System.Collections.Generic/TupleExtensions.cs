ebc6fdd35907b6443748577e2dd55018949a6347
Hendrik Bulens <hendrik.bulens@gmail.com>
2020-11-20 12:20:48 +0100

    Move tuple extension from DS

---
+++ src/core/System.Collections.Generic/TupleExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace System.Collections.Generic
+{
+    public static class TupleExtensions
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="tuples"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static IEnumerable<TValue> Where<TKey, TValue>(this IEnumerable<Tuple<TKey, TValue>> tuples, TKey value)
+            => tuples.Where(x => x.Item1.Equals(value)).Select(x => x.Item2);
+    }
+}
---
+++ src/test/TupleExtensionsTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Dime.Utilities.Tests
+{
+    public class TupleExtensionsTests
+    {
+        private class MyClass
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        [Fact]
+        [Trait("Category", "Tuple")]
+        public void TupleExtensions_Where_FiltersTuple()
+        {
+            IEnumerable<Tuple<int, MyClass>> tuple = new List<Tuple<int, MyClass>>
+            {
+                new Tuple<int,MyClass>(1, new MyClass { Id = 1 }),
+                new Tuple<int,MyClass>(2, new MyClass { Id = 2 }),
+                new Tuple<int,MyClass>(2, new MyClass { Id = 3 })
+            };
+
+            IEnumerable<MyClass> tuples = tuple.Where(2);
+            Assert.True(tuples.Count() == 2);
+        }
+    }
+}
