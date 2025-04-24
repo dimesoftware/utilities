5937d57a3ad005de9eaebdebb54885416bd337e2
Hendrik Bulens <hendrik.bulens@gmail.com>
2025-04-24 12:45:26 +0200

    Move repo newtonsoft-json-extensions

---
+++ src/serialization/Dime.Utilities.Serialization/Copier.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+
+namespace Dime.Utilities.Serialization
+{
+    /// <summary>
+    /// Reference Article http://www.codeproject.com/KB/tips/SerializedObjectCloner.aspx
+    /// Provides a method for performing a deep copy of an object.
+    /// Binary Serialization is used to perform the copy.
+    /// </summary>
+    public static class Copier
+    {
+        /// <summary>
+        /// Perform a deep copy of the object, using JSON as a serialization method.
+        /// </summary>
+        /// <typeparam name="T">The type of object being copied.</typeparam>
+        /// <param name="source">The object instance to copy.</param>
+        /// <returns>The copied object.</returns>
+        public static T DeepClone<T>(this T source)
+        {
+            // Initialize inner objects individually
+            // For example in default constructor some list property initialized with some values,
+            // but in 'source' these items are cleaned - without ObjectCreationHandling.Replace default constructor values will be added to result
+            JsonSerializerSettings deserializeSettings = new JsonSerializerSettings {
+                ObjectCreationHandling = ObjectCreationHandling.Replace,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            };
+
+            return source.DeepClone(deserializeSettings);
+        }
+
+        /// <summary>
+        /// Perform a deep copy of the object, using JSON as a serialization method.
+        /// </summary>
+        /// <typeparam name="T">The type of object being copied.</typeparam>
+        /// <param name="source">The object instance to copy.</param>
+        /// <param name="settings">The serialization settings</param>
+        /// <returns>The copied object.</returns>
+        public static T DeepClone<T>(this T source, JsonSerializerSettings settings)
+            => source == null ? default : JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(source, settings), settings);
+    }
+}
---
+++ src/serialization/Dime.Utilities.Serialization/JsonConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Dime.Utilities.Serialization
+{
+    public static class JsonConverter
+    {
+        public static T TryDeserializeObject<T>(string value, Type type) where T : class
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject(value, type) as T;
+            }
+            catch (Exception)
+            {
+                return default;
+            }
+        }
+    }
+}
